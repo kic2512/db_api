3bd8c0b851d7542f49573c9f2af3af19b4fecdbf
Kic <kic@kic-Lenovo-B450.(none)>
2014-10-12 21:03:41 +0400

    refactor 1

--- forum_app/my_test.py
+++ forum_app/my_test.py
@@ -7,14 +7,14 @@ import json
 def forum_create():
     dict = {'name': 'Forum With Sufficiently Large Name', 'short_name': 'forumwithsufficientlylargename',
             'user': 'test@test.com'}
-    host = "http://127.0.0.1/db/api/user/create/"
+    host = "http://127.0.0.1/db/api/forum/create/"
     response = requests.post(host, data=json.dumps(dict), headers={'content-type': 'application/json'})
     print response.text
 
 
 def user_create():
-    dict = {"username": "user1", "about": "hello im user1", "isAnonymous": False, "name": "John",
-            "email": "example@mail.ru"}
+    dict = {'username': 'user1', 'about': 'hello im user1', 'isAnonymous': False, 'name': 'John',
+            'email': 'example@mail.ru'}
     host = "http://127.0.0.1/db/api/user/create/"
     response = requests.post(host, data=json.dumps(dict), headers={'content-type': 'application/json'})
     print response.text
