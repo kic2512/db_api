1c4cf9c2f87d02e7799a698133020813d2ee0123
Kic <kic@kic-Lenovo-B450.(none)>
2015-01-23 20:35:29 +0400

    thread unsubscribe

--- forum_app/api_packs/thread_api/thread_unsubscribe.py
+++ forum_app/api_packs/thread_api/thread_unsubscribe.py
@@ -2,7 +2,7 @@ __author__ = 'kic'
 
 import flask
 from forum_app.api_packs.db_queries.queries import exec_sql, open_sql, build_sql_select_all_query, \
-    build_sql_update_query
+    build_sql_update_query, open_sql_all
 from forum_app.api_packs.make_response.make_response import make_response
 
 
@@ -22,7 +22,12 @@ def unsubscribe_thread(data):
 
     sql_check = build_sql_select_all_query(sql_scheme)
 
-    res = open_sql(sql_check)  # check if exists
+    res_dict = open_sql_all(sql_check, first=True, is_closing=False)  # check if exists
+    res = res_dict['result'][0]
+    if res == -1 or not res:
+        return make_response([], [], code=4)
+    db = res_dict['db']
+    crs = res_dict['cursor']
 
     if res:
         sql_scheme = {
@@ -31,7 +36,7 @@ def unsubscribe_thread(data):
             'condition': {'id': res['id']},
             'table': 'Subscribe'
         }
-        exec_sql(build_sql_update_query(sql_scheme))
+        exec_sql(build_sql_update_query(sql_scheme), first=False, cursor=crs, is_closing=False)
 
         keys = ['thread', 'user']
 
@@ -40,5 +45,6 @@ def unsubscribe_thread(data):
     else:
         code = 1
 
+    db.close()
     resp_dict = make_response(keys=keys, values=values, code=code)
     return flask.jsonify(resp_dict)
