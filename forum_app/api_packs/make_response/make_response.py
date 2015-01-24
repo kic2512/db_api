a3c95c0faea33af1ef24b66485146443ee3b97b8
Kic <kic@kic-Lenovo-B450.(none)>
2015-01-24 10:33:53 +0400

    fix error response

--- forum_app/api_packs/make_response/make_response.py
+++ forum_app/api_packs/make_response/make_response.py
@@ -5,14 +5,6 @@ from datetime import datetime
 
 def make_response(keys=[], values=[], code=0, sql='no'):
     if code != 0:
-
-        f = open('/home/kic/flask_proj/forum_app/api_packs/make_response/api.log', 'a')
-        f.write(str(datetime.now()))
-        f.write(' : ')
-        f.write(sql)
-        f.write('\n')
-        f.close()
-
         response = {'code': code, 'response': ERRORS_MESSAGES[code]}
         return response
 
